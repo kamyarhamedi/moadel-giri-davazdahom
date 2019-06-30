e0a7b486d28b3b1ae72ad4df2c44178bbee72d57
kamyar hamedi <52383905+kamyarhamedi@users.noreply.github.com>
2019-06-30 23:50:42 +0430

    Add files via upload

---
+++ moadel.py
@@ -0,0 +1,141 @@
+small = '#################################'
+tall = '********************************'
+ball='@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
+
+a = 20.0 
+print('anzebat:' + str(a))
+a_zarib = a * 2
+print('anzebat + zarib:' + str(a_zarib))
+print(tall)
+
+b = 20.0
+print('dini:' + str(b))
+b_zarib = b * 2
+print('dini + zarib:' + str(b_zarib))
+print(tall)
+
+c = 20.0
+print('arabi' + str(c))
+c_zarib = c * 2
+print ('arabi + zarib:' + str (c_zarib))
+print(tall)
+
+d = 20.0
+print('adabiat:' + str(d))
+d_zarib = d * 2
+print('adabiat + zarib:' + str(d_zarib))
+print(tall)
+
+e = 20.0
+print('ensha:' + str(e))
+e_zarib = e * 2
+print('ensha + zarib:' + str(e_zarib))
+print(tall)
+
+f = 20.0
+print('zaban:' + str(f))
+f_zarib = f * 4 
+print('zaban + zarib:' + str(f_zarib))
+print(tall)
+
+g = 20.0
+print('varzesh:' + str(g))
+g_zarib = g * 2
+print('varzesh + zarib:' + str(g_zarib))
+print(tall)
+
+h = 20.0
+print('shimi:' + str(h))
+h_zarib = h * 4
+print('shimi + zarib:' + str(h_zarib))
+print(tall)
+
+i = 20.0
+print('salamatvabehdasht:' + str(i))
+i_zarib = i * 2
+print('salamatvabehdasht + zarib:' + str(i_zarib))
+print(tall)
+
+j = 20.0
+print('modereatekhanevadeh:' + str(j))
+j_zarib = j * 2
+print('modereatekhanevadeh + zarib:' + str(j_zarib))
+print(tall)
+
+k = 20.0 
+print('hovetejtemaee:' + str (k))
+k_zarib = k * 2
+print('hovetejtemaee + zarib:' + str(k_zarib))
+print(tall)
+
+l = 20.0
+print('zishtshenasi:' + str(l))
+l_zarib = l * 4
+print('zishtshenasi + zarib:' + str(l_zarib))
+print(tall)
+
+m = 20.0
+print('riazi:' + str(m))
+m_zarib = m * 4
+print('riazi + zarib:' + str(m_zarib))
+print(tall)
+
+n = 20.0
+print('fizik:' + str(n))
+n_zarib = n * 3
+print('fizik + zarib:' + str(n_zarib))
+print(tall)
+print(tall)
+
+print(small)
+print(small)
+
+jamemoadel = a_zarib + b_zarib + c_zarib + d_zarib + e_zarib + f_zarib + g_zarib + h_zarib + i_zarib + j_zarib + k_zarib + l_zarib + m_zarib + n_zarib  
+print('jamemoadel:' + str(jamemoadel))
+
+zaribkol = 37
+print('zaribkol:' + str(zaribkol))
+
+moadelkol = jamemoadel / zaribkol
+print('moadelkol=jamemoadel/zaribkol:' + str(moadelkol))
+
+print(small)
+print(small)
+
+print(ball)
+
+print('a:' + str(type(a_zarib)))
+
+print('b:' + str(type(b_zarib)))
+
+print('c:' + str(type(c_zarib)))
+
+print('d:' + str(type(d_zarib)))
+
+print('e:' + str(type(e_zarib)))
+
+print('f:' + str(type(f_zarib)))
+
+print('g:' + str(type(g_zarib)))
+
+print('h:' + str(type(h_zarib)))
+
+print('i:' + str(type(i_zarib)))
+
+print('j:' + str(type(j_zarib)))
+
+print('k:' + str(type(k_zarib)))
+
+print('l:' + str(type(l_zarib)))
+
+print('m:' + str(type(m_zarib)))
+
+print('n:' + str(type(n_zarib)))
+
+print('moadelkol:' + str(type(moadelkol)))
+
+print('zaribkol:' + str(type(zaribkol)))
+
+
+
+
